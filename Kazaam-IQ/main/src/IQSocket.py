477638d804aebab83a412f8155764b55c42e3cf2
jecker7 <jhecker123@gmail.com>
2019-10-27 14:58:40 -0400

    adding a base socket subclassing thread

--- Kazaam-IQ/main/src/IQSocket.py
+++ Kazaam-IQ/main/src/IQSocket.py
@@ -1,9 +1,39 @@
-import _socket
+import socket
+import threading
 
-class IQSocket(socket.socket):
+class IQSocket(socket.socket, threading.Thread):
     def __init__(self, *args, **kwargs):
-        super(MySocket, self).__ini__(*args, **kwargs)
-        self.queue = queue.Queue
+        super(IQSocket, self).__init__(*args, **kwargs)
 
-    def connect_socket(self, int, port):
+    def run(self):
+        pass
+
+    def connect(self, host, port):
         try:
+            self.bind((host, port))
+            self.listen()
+            conn, addr = self.accept()
+            with conn:
+                print("Connected by ", addr)
+            while True:
+                data = conn.recv(1024)
+                print(data)
+                if not data:
+                    break
+        except:
+            print("Error binding to IQ Service")
+
+
+    def disconnect(self):
+        try:
+            self.disconnect()
+        except:
+            print("Error disconnecting.")
+
+    def read(self):
+        while self.
+
+if __name__ == "__main__":
+    t = IQSocket()
+    t.start()
+    print("alive: " + t.is_alive())
