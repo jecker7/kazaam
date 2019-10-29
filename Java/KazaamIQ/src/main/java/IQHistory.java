a4360c33de5f75ca05670b90bdc00df8c9a8d592
jecker <john.ecker@amadeus.com>
2019-10-29 14:51:09 -0400

    adding historical interface

---
+++ Java/KazaamIQ/src/main/java/IQHistory.java
@@ -0,0 +1,65 @@
+
+public class IQHistory{
+
+    private int iPort;
+    private IQSocket historySocket;
+
+    public IQHistory(int iPort){
+        this.iPort = iPort;
+    }
+
+    public void prepare(){
+        this.historySocket.ConnectSocket(this.iPort);
+        System.out.println("Connection status : " + this.historySocket.isConnected());
+        this.historySocket.CreateBuffers();
+        try {
+            this.historySocket.writer.write();
+        }
+    }
+
+    public void requestData(String request){
+        try {
+
+        }
+    }
+
+    public void setProtocol(String version){
+
+    }
+
+    public void setClientName(String name){
+
+    }
+
+    public String HTX(String symbol, String maxPts){
+        return String.format("HTX,%s,%s\r\n", symbol, maxPts);
+    }
+    public String HTD(String symbol, String maxDays){
+        return String.format("HTD,%s,%s\r\n", symbol, maxDays);
+    }
+    public String HTT(String symbol, String begin, String end){
+        return String.format("HTT,%s,%s,%s\r\n", symbol, begin, end);
+    }
+    public String HIX(String symbol, String interval, String maxData){
+        return String.format("HIX,%s,%s,%s\r\n", symbol, interval, maxData);
+    }
+    public String HID(String symbol, String interval, String maxDays){
+        return String.format("HID,%s,%s,%s\r\n", symbol, interval, maxDays);
+    }
+    public String HIT(String symbol, String interval, String begin, String end){
+        return String.format("HID,%s,%s,%s,%s\r\n", symbol, interval, begin,end);
+    }
+    public String HDX(String symbol, String maxDays){
+        return String.format("HID,%s,%s\r\n", symbol, maxDays);
+    }
+    public String HDT(String symbol, String begin, String end){
+        return String.format("HID,%s,%s,%s\r\n", symbol, begin, end);
+    }
+    public String HWX(String symbol, String maxWeeks){
+        return String.format("HID,%s,%s\r\n", symbol, maxWeeks);
+    }
+    public String HMX(String symbol, String maxMonths){
+        return String.format("HID,%s,%s\r\n", symbol, maxMonths);
+    }
+}
+
---
+++ Java/KazaamIQ/src/main/java/IQSocket.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.net.*;
+
+public class IQSocket extends Socket {
+    BufferedReader reader;
+    BufferedWriter writer;
+
+    public IQSocket(){
+        super();
+    }
+
+    protected boolean ConnectSocket(int iPort){
+        try{
+            this.connect(new InetSocketAddress("localhost", iPort));
+        } catch (Exception e){
+            System.out.println("Unable to connect");
+            e.printStackTrace();
+        } finally {
+            return this.isConnected();
+        }
+    }
+
+    protected void Disconnect(){
+        try {
+            this.CloseBuffers();
+            this.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    protected void CreateBuffers(){
+        try {
+            reader = new BufferedReader(new InputStreamReader(this.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(this.getOutputStream()));
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Unable to create buffers");
+        }
+    }
+
+    protected void CloseBuffers(){
+        try {
+            reader.close();
+            writer.close();
+        } catch (Exception e){
+            System.out.println("Unable to close buffers");
+            e.printStackTrace();
+        }
+    }
+}
